3bb2fca3ce0587806c200f86636d27a2ab745629
meijie555 <1582384970@qq.com>
2019-10-27 22:34:03 +0800

    首页-侧边栏完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,47 @@
 <template>
+  <!-- 整体容器 -->
   <el-container class="home-container">
-    <el-aside width="200px">Aside</el-aside>
+    <!-- 侧边栏 -->
+    <el-aside width="200px">
+      <!-- logo -->
+      <div class="logo"></div>
+      <el-menu
+        default-active="1"
+        background-color="#002033"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+        style="border-right:none"
+      >
+        <el-menu-item index="1">
+          <i class="el-icon-s-home"></i>
+          <span slot="title">首页</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-document"></i>
+          <span slot="title">内容管理</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-picture"></i>
+          <span slot="title">素材管理</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-s-promotion"></i>
+          <span slot="title">发布文章</span>
+        </el-menu-item>
+        <el-menu-item index="5">
+          <i class="el-icon-chat-dot-round"></i>
+          <span slot="title">评论管理</span>
+        </el-menu-item>
+        <el-menu-item index="6">
+          <i class="el-icon-present"></i>
+          <span slot="title">粉丝管理</span>
+        </el-menu-item>
+        <el-menu-item index="7">
+          <i class="el-icon-setting"></i>
+          <span slot="title">个人设置</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
     <el-container>
       <!-- 头部 -->
       <el-header>
@@ -16,8 +57,8 @@
             <i class="el-icon-arrow-down el-icon--right"></i>
           </span>
           <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>个人设置</el-dropdown-item>
-            <el-dropdown-item>退出登录</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-setting">个人设置</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-unlock">退出登录</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
       </el-header>
@@ -39,6 +80,12 @@ export default {}
   top: 0;
   .el-aside {
     background: #002033;
+    .logo {
+      width: 100%;
+      height: 60px;
+      background: #002244 url(../../assets/logo_admin.png) no-repeat center /
+        140px auto;
+    }
   }
   .el-header {
     border-bottom: 1px solid #ddd;
