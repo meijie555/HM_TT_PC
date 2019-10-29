82b2a34f632a7f75b0bc80e969ba2e9f62e7b0ec
meijie555 <1582384970@qq.com>
2019-10-29 10:32:25 +0800

    axios-响应拦截器

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import local from '../utils/local'
+import router from '../router'
 // 配置axios  baseurl  作用：设置基准地址 （前面相同的一段地址）
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
 
@@ -18,4 +19,17 @@ axios.interceptors.request.use(function (config) {
   // 错误信息处理
   return Promise.reject(error)
 })
+
+// 响应拦截器
+axios.interceptors.response.use(function (response) {
+  // 发送请求前做什么
+  return response
+}, function (error) {
+  // 错误信息处理
+  if (error.response.status === 401) {
+    return router.push('/login')
+  }
+  return Promise.reject(error)
+})
+
 export default axios
