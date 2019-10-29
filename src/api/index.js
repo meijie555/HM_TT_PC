5419b3b0c46813d9b184f37b7d9cbfb9259f043d
meijie555 <1582384970@qq.com>
2019-10-29 10:25:49 +0800

    axios-请求拦截器

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,18 @@ import local from '../utils/local'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
 
 // 配置请求头
-if (local.getUser()) {
-  axios.defaults.headers.Authorization = `Bearer ${local.getUser().token}`
-}
+// if (local.getUser()) {
+//   axios.defaults.headers.Authorization = `Bearer ${local.getUser().token}`
+// }
+
+// 请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 发送请求前做什么
+  const user = local.getUser() || {}
+  config.headers.Authorization = `Bearer ${user.token}`
+  return config
+}, function (error) {
+  // 错误信息处理
+  return Promise.reject(error)
+})
 export default axios
