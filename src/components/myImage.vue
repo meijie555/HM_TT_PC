1e734dbcc02c5b66ae9c90ab83334bc06e6b6a16
meijie555 <1582384970@qq.com>
2019-11-04 11:22:58 +0800

    发布文章-封面组件-确认图片

--- src/components/myImage.vue
+++ src/components/myImage.vue
@@ -2,7 +2,7 @@
   <div class="my-image">
     <!-- 图片式按钮 -->
     <div class="btn_box" @click="open">
-      <img src="../assets/default.png" alt />
+      <img :src="defaultImage" alt />
     </div>
     <!-- 对话框 -->
     <el-dialog :visible.sync="dialogVisible" width="750px">
@@ -18,7 +18,13 @@
           </div>
           <!-- 素材  -->
           <div class="imgList">
-            <div class="imgItem" v-for="item of images" :key="item.id">
+            <div
+              class="imgItem"
+              v-for="item of images"
+              :key="item.id"
+              @click="selectedImage(item.url)"
+              :class="{selected:selectedImageUrl===item.url}"
+            >
               <img :src="item.url" />
             </div>
           </div>
@@ -32,17 +38,31 @@
             @current-change="pager"
           ></el-pagination>
         </el-tab-pane>
-        <el-tab-pane label="上传图片" name="upload">上传图片</el-tab-pane>
+        <el-tab-pane label="上传图片" name="upload">
+          <el-upload
+            class="avatar-uploader"
+            action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
+            :headers="headers"
+            name="image"
+            :show-file-list="false"
+            :on-success="handleSuccess"
+          >
+            <img v-if="uplocalImageUrl" :src="uplocalImageUrl" class="avatar" />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </el-tab-pane>
       </el-tabs>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="confirmImage">确 定</el-button>
       </span>
     </el-dialog>
   </div>
 </template>
 
 <script>
+import local from '@/utils/local'
+import defaultImage from '../assets/default.png'
 export default {
   name: 'my-image',
   data () {
@@ -55,7 +75,13 @@ export default {
       dialogVisible: false,
       activeName: 'image',
       images: [],
-      total: 0
+      total: 0,
+      selectedImageUrl: null,
+      headers: {
+        Authorization: `Bearer ${local.getUser().token}`
+      },
+      uplocalImageUrl: null,
+      defaultImage
     }
   },
   methods: {
@@ -63,6 +89,9 @@ export default {
     open () {
       this.dialogVisible = true
       this.getImage()
+      // this.selectedImageUrl = null
+      this.uplocalImageUrl = null
+      this.activeName = 'image'
     },
     // 切换全部与收藏
     toggleList () {
@@ -81,6 +110,30 @@ export default {
     pager (newpage) {
       this.reqParams.page = newpage
       this.getImage()
+    },
+    selectedImage (url) {
+      this.selectedImageUrl = url
+    },
+    handleSuccess (res) {
+      // console.log(res)
+      // 预览
+      this.uplocalImageUrl = res.data.url
+      this.$message.success('添加成功')
+    },
+    confirmImage () {
+      if (this.activeName === 'image') {
+        if (!this.selectedImageUrl) {
+          return this.$message.warning('请选择图片')
+        }
+        this.defaultImage = this.selectedImageUrl
+        this.dialogVisible = false
+      } else {
+        if (!this.uplocalImageUrl) {
+          return this.$message.warning('请上传图片')
+        }
+        this.defaultImage = this.uplocalImageUrl
+        this.dialogVisible = false
+      }
     }
   }
 }
@@ -117,6 +170,16 @@ export default {
     img {
       width: 100%;
       height: 100%;
+    }
+    &.selected::after {
+      content: "";
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      background: rgba(0, 0, 0, 0.3) url(../assets/selected.png) no-repeat
+        center / 50px 50px;
     }
   }
 }
