301376d51b3e472d79adf64711c3f0b647392131
meijie555 <1582384970@qq.com>
2019-11-06 22:12:49 +0800

    (粉丝管理完成

--- src/views/fans/index.vue
+++ src/views/fans/index.vue
@@ -10,14 +10,14 @@
           <div class="fans_list">
             <div class="fans_item" v-for="item of fans" :key="item.id.toString()">
               <!-- 头像 -->
-               <el-avatar :size="80" :src="item.photo"></el-avatar>
+              <el-avatar :size="80" :src="item.photo"></el-avatar>
               <!-- 用户名 -->
               <p>{{item.name}}</p>
               <!-- 关注 -->
-              <el-button type='primary' plain size="small">+关注</el-button>
+              <el-button type="primary" plain size="small">+关注</el-button>
             </div>
           </div>
-           <!-- 分页 -->
+          <!-- 分页 -->
           <el-pagination
             background
             layout="prev, pager, next"
@@ -27,18 +27,21 @@
             @current-change="pager"
           ></el-pagination>
         </el-tab-pane>
-        <el-tab-pane label="粉丝画像" name="fansPhoto">粉丝画像</el-tab-pane>
+        <el-tab-pane label="粉丝画像" name="fansPhoto">
+          <div ref="dom" style="width: 600px;height:400px;"></div>
+        </el-tab-pane>
       </el-tabs>
     </el-card>
   </div>
 </template>
 
 <script>
+import echarts from 'echarts'
 import circleUrl from '../../assets/avatar.jpg'
 export default {
   data () {
     return {
-      activeName: 'fansList',
+      activeName: 'fansPhoto',
       // 默认头像
       circleUrl,
       fans: [],
@@ -52,9 +55,53 @@ export default {
   created () {
     this.getFansList()
   },
+  mounted () {
+    const dom = this.$refs.dom
+    const myEcharts = echarts.init(dom)
+    var option = {
+      color: ['#3398DB'],
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: { // 坐标轴指示器，坐标轴触发有效
+          type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+          axisTick: {
+            alignWithLabel: true
+          }
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
+      series: [
+        {
+          name: '直接访问',
+          type: 'bar',
+          barWidth: '60%',
+          data: [10, 52, 200, 334, 390, 330, 220]
+        }
+      ]
+    }
+    myEcharts.setOption(option)
+  },
   methods: {
     async getFansList () {
-      const { data: { data } } = await this.$axios.get('followers', { params: this.reqParams })
+      const {
+        data: { data }
+      } = await this.$axios.get('followers', { params: this.reqParams })
       this.fans = data.results
       this.total = data.total_count
     },
