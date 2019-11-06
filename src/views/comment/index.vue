759a98d758e57c21003e7ff2da4df958b8eef65b
meijie555 <1582384970@qq.com>
2019-11-06 09:58:27 +0800

    评论管理-列表与分页

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class='container'>
+  <div class="container">
     <el-card>
       <div slot="header">
         <my-Breadcrumb>评论管理</my-Breadcrumb>
       </div>
       <!-- 表格 -->
       <el-table :data="comments">
-        <el-table-column label="标题" width="400px"></el-table-column>
-        <el-table-column label="总评论数"></el-table-column>
-        <el-table-column label="粉丝评论数"></el-table-column>
-        <el-table-column label="状态"></el-table-column>
-        <el-table-column label="操作" width="120px"></el-table-column>
+        <el-table-column label="标题" width="400px" prop="title"></el-table-column>
+        <el-table-column label="总评论数" prop="total_comment_count"></el-table-column>
+        <el-table-column label="粉丝评论数" prop="fans_comment_count"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">{{scope.row.comment_status?'正常' : '关闭'}}</template>
+        </el-table-column>
+        <el-table-column label="操作" width="120px">
+          <template slot-scope="scope">
+            <el-button v-if="scope.row.comment_status" type="danger" size="small">关闭评论</el-button>
+            <el-button v-else type="success" size="small">打开评论</el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <!-- 分页 -->
+      <el-pagination
+        style="margin-top:20px"
+        background
+        layout="prev, pager, next"
+        :total="total"
+        :page-size="reqParams.per_page"
+        :current-page="reqParams.page"
+        @current-change="pager"
+      ></el-pagination>
     </el-card>
   </div>
 </template>
@@ -21,7 +37,32 @@
 export default {
   data () {
     return {
-      comments: []
+      comments: [],
+      reqParams: {
+        response_type: 'comment',
+        page: 1,
+        per_page: 20
+      },
+      total: 0
+    }
+  },
+  created () {
+    this.getComments()
+  },
+  methods: {
+    async getComments () {
+      const {
+        data: { data }
+      } = await this.$axios.get('articles', { params: this.reqParams })
+      this.comments = data.results
+      this.total = data.total_count
+    },
+    // 分页函数
+    pager (newPage) {
+      // 修改当前的页码为新的页码
+      this.reqParams.page = newPage
+      // 重新获取数据
+      this.getComments()
     }
   }
 }
